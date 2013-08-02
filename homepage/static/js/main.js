7b1ac55cb028f7c884a03d9902eec21d7c93aff5
Mykola Kharechko <crchemist@gmail.com>
2013-08-03 00:27:35 +0300

    #1 finished with client-side

--- homepage/static/js/main.js
+++ homepage/static/js/main.js
@@ -1,4 +1,10 @@
 function ContactViewModel() {
+    var self = this;
+    self.user_data = ko.observable();
+
+    $.getJSON('/contact/', function(data) {
+        self.user_data(data)
+    });
 }
 
 ko.applyBindings(new ContactViewModel())
