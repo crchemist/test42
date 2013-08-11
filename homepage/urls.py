e8dab48272d07374cc000239c61fb7b9df85a803
Mykola Kharechko <crchemist@gmail.com>
2013-08-11 12:45:35 +0300

    ticket:1 make code pep8 compatible

--- homepage/urls.py
+++ homepage/urls.py
@@ -8,7 +8,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', 'homepage.contacts.views.index'),
     url(r'^contact/$', include('homepage.contacts.urls')),
 
