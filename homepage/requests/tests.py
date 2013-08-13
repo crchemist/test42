a8d114927585246b8d39298d719bc6193a51a047
Mykola Kharechko <crchemist@gmail.com>
2013-08-13 20:09:16 +0300

    ticket:3 added unittests for middleware

--- homepage/requests/tests.py
+++ homepage/requests/tests.py
@@ -1,13 +1,16 @@
 from __future__ import absolute_import
 
 import json
+from StringIO import StringIO
 
 from django.core.urlresolvers import reverse
+from django.core.handlers.wsgi import WSGIRequest
 
 from django.test import TestCase
 from django.test.client import Client
 
 from .models import Request
+from .middleware import RequestsStoreMiddleware
 
 
 class RequestsMiddlewareTest(TestCase):
@@ -23,6 +26,15 @@ class RequestsMiddlewareTest(TestCase):
 
         c.get('/some_url')
         self.assertEqual(Request.objects.all().count(), 1)
+
+    def test_requests_midleware_class(self):
+        requests_middleware = RequestsStoreMiddleware()
+        path = '/test-url2'
+        req = WSGIRequest({'REQUEST_METHOD': 'GET',
+                           'PATH_INFO': path,
+                           'wsgi.input': StringIO()})
+        requests_middleware.process_request(req)
+        self.assertEqual(Request.objects.filter(url=path).count(), 1)
 
     def test_get_requests(self):
         c = Client()
