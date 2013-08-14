7398250bcfe0977facf5b1045f636be16050ab78
Mykola Kharechko <crchemist@gmail.com>
2013-08-14 22:03:08 +0300

    ticket:5 added edit_link tag and tests for him

---
+++ homepage/contacts/templatetags/edit_link.py
@@ -0,0 +1,15 @@
+from django import template
+from django.core.urlresolvers import reverse
+from django.contrib.contenttypes.models import ContentType
+
+register = template.Library()
+
+
+def edit_link(obj):
+    """Usage: {% edit_link some_obj %}
+    """
+    obj_ct = ContentType.objects.get_for_model(obj.__class__)
+    return reverse('admin:%s_%s_change' % (obj_ct.app_label, obj_ct.name),
+                args=(obj.id,))
+
+register.simple_tag(edit_link)
